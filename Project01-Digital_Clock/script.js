508e00e5172b4108bf5d908f9a5b27fc96192400
Vibhu Sahu <vibhu6751@gmail.com>
2024-04-05 17:15:33 +0530

    done some correction in Project 1

--- Project01-Digital_Clock/script.js
+++ Project01-Digital_Clock/script.js
@@ -1,18 +1,22 @@
-const hourE = document.createElement("hour");
+// Removed unnecessary variable 'options'
+
+// Corrected the element creation for the 'hour' span
+const hourE = document.getElementById("hour");
 const minuteE = document.getElementById("minute");
 const secondE = document.getElementById("second");
 const ampmE = document.getElementById("ampm");
+
 function TimeCall() {
-  let h = new Date().getHours;
-  let m = new Date().getMinutes;
-  let s = new Date().getSeconds;
+  // Corrected method calls to get current time
+  let h = new Date().getHours(); // Corrected method call to getHours()
+  let m = new Date().getMinutes(); // Corrected method call to getMinutes()
+  let s = new Date().getSeconds(); // Corrected method call to getSeconds()
   let ampm = "AM";
 
   if (h > 12) {
     h -= 12;
     ampm = "PM";
   }
-
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
@@ -22,8 +26,6 @@ function TimeCall() {
   secondE.innerText = s;
   ampmE.innerText = ampm;
 
-  setTimeout(function () {
-    TimeCall();
-  }, 1000);
+  setTimeout(TimeCall, 1000); // Removed unnecessary anonymous function wrapper
 }
 TimeCall();
